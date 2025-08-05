13e4e8cfdd679d20fd393b93258aa26ed48afb23
sagen89 <113891754+sagen89@users.noreply.github.com>
2025-08-06 00:15:02 +0300

    init

---
+++ src/main/java/com/mycompany/spring_usboscilloscope/SpringUsboscilloscopeApplication.java
@@ -0,0 +1,13 @@
+package com.mycompany.spring_usboscilloscope;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringUsboscilloscopeApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpringUsboscilloscopeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mycompany/spring_usboscilloscope/SpringUsboscilloscopeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycompany.spring_usboscilloscope;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringUsboscilloscopeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
